5c2641700be1a37bd1aa26139169bec6f718c4f5
Ankit Yadav <yadavankit840@gmail.com>
2020-10-14 20:08:51 +0530

    Navigation controllers added to all scenes.

--- HCI/View Controllers/TabBarVC/ComplaintSubVC/ComplainRegisteredVC.swift
+++ HCI/View Controllers/TabBarVC/ComplaintSubVC/ComplainRegisteredVC.swift
@@ -15,7 +15,7 @@ class ComplainRegisteredVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         gifImageView.loadGif(name: "fire")
-        cardView.layer.cornerRadius = 20
+        cardView.layer.cornerRadius = 38
     }
 
 }
